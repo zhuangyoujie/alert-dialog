9f7eddc61a169a10fe5e2ef86549a8ff2da61838
庄友杰 <zdy_1985@126.com>
2017-04-19 17:41:29 +0800

    添加confirm

--- alert.js
+++ alert.js
@@ -1,23 +1,45 @@
 function Alert() {
     this.cfg = {
-        width:200,
-        height:200,
-        title:'',
-        content:''
+        title:'标题',
+        content:'hekkkkkkaskkkkkkkkk',
+        handler:""
     }
 }
 Alert.prototype ={
     alert:function (cfg) {
        var CFG = $.extend(this.cfg,cfg);
-       var boundingBox =  $('<div class="mask"></div>'+
-           '<div class="alert_boundingBox">' +
-           '<div class="alert_hd">' + CFG.title + '</div>' +
+       var boundingBox =  $('<div class="mask animate-fade-in"></div>'+
+           '<div class="alert_boundingBox animate-fade-in">' +
+           '<div class="alert_hd"><strong class="alert_title">' + CFG.title + '</strong></div>' +
            '<div class="alert_bd">' + CFG.content + '</div>' +
-           '<div class="alert_ft"><a href="javascript:;">确定</a></div>' +
+           '<div class="alert_ft"><a href="javascript:;" class="alert_btn_primary">确定</a></div>' +
            '</div>');
         boundingBox.appendTo('body');
-        boundingBox.css({
-
+        var btn = boundingBox.find('alert_btn_primary');
+        btn.click(function(){
+            CFG.handler && CFG.handler();
+            boundingBox.remove();
         });
+    },
+    confirm:function (cfg) {
+        var CFG = $.extend(this.cfg,cfg);
+        var boundingBox =  $('<div class="mask animate-fade-in"></div>'+
+            '<div class="alert_boundingBox animate-fade-in">' +
+            '<div class="alert_hd"><strong class="alert_title">' + CFG.title + '</strong></div>' +
+            '<div class="alert_bd">' + CFG.content + '</div>' +
+            '<div class="alert_ft"><a href="javascript:;" class="alert_btn alert_btn_default ">取消</a><a href="javascript:;" class="alert_btn alert_btn_primary">确定</a></div>' +
+            '</div>');
+        boundingBox.appendTo('body');
+        var cancelBtn = boundingBox.find('.alert_btn_default');
+        var ensureBtn = boundingBox.find('.alert_btn_primary');
+        ensureBtn.click(function(){
+            CFG.handler && CFG.handler();
+            boundingBox.remove();
+        });
+        cancelBtn.click(function(){
+            CFG.handler && CFG.handler();
+            boundingBox.remove();
+        });
+
     }
-}
+};
