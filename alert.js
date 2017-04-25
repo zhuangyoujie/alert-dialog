a95763a5dd83887d5f70721c3f809e281d8c51a2
庄友杰 <zdy_1985@126.com>
2017-04-25 14:34:40 +0800

    修改弹框样式

--- alert.js
+++ alert.js
@@ -1,23 +1,57 @@
 function Alert() {
     this.cfg = {
-        width:200,
-        height:200,
-        title:'',
-        content:''
+        title:'标题',
+        content:'hekkkkkkaskkkkkkkkk',
+        handler:"",
+        hasMask:true
     }
 }
 Alert.prototype ={
     alert:function (cfg) {
-       var CFG = $.extend(this.cfg,cfg);
-       var boundingBox =  $('<div class="mask"></div>'+
-           '<div class="alert_boundingBox">' +
-           '<div class="alert_hd">' + CFG.title + '</div>' +
-           '<div class="alert_bd">' + CFG.content + '</div>' +
-           '<div class="alert_ft"><a href="javascript:;">确定</a></div>' +
-           '</div>');
+        var CFG = $.extend(this.cfg,cfg);
+        var boundingBox =  $(
+            '<div class="alert_boundingBox animate-fade-in">' +
+            '<div class="alert_hd"><strong class="alert_title">' + CFG.title + '</strong></div>' +
+            '<div class="alert_bd">' + CFG.content + '</div>' +
+            '<div class="alert_ft"><a href="javascript:;" class="alert_btn alert_btn_primary">确定</a></div>' +
+            '</div>');
         boundingBox.appendTo('body');
-        boundingBox.css({
-
+        if(CFG.hasMask){
+            var mask = $('<div class="jui_mask animate-fade-in"></div>');
+            mask.appendTo('body');
+        }
+        var ensureBtn = boundingBox.find('.alert_btn_primary');
+        ensureBtn.click(function(){
+            CFG.handler && CFG.handler();
+            boundingBox.remove();
+            mask.remove();
         });
+    },
+    confirm:function (cfg) {
+        var CFG = $.extend(this.cfg,cfg);
+        var boundingBox =  $(
+            '<div class="alert_boundingBox animate-fade-in">' +
+            '<div class="alert_hd"><strong class="alert_title">' + CFG.title + '</strong></div>' +
+            '<div class="alert_bd">' + CFG.content + '</div>' +
+            '<div class="alert_ft"><a href="javascript:;" class="alert_btn alert_btn_default ">取消</a><a href="javascript:;" class="alert_btn alert_btn_primary">确定</a></div>' +
+            '</div>');
+        boundingBox.appendTo('body');
+        if(CFG.hasMask){
+            var mask = $('<div class="jui_mask animate-fade-in"></div>');
+            mask.appendTo('body');
+        }
+        var cancelBtn = boundingBox.find('.alert_btn_default');
+        var ensureBtn = boundingBox.find('.alert_btn_primary');
+        ensureBtn.click(function(){
+            CFG.handler && CFG.handler();
+            boundingBox.remove();
+            mask.remove();
+        });
+        cancelBtn.click(function(){
+            CFG.handler && CFG.handler();
+            boundingBox.remove();
+            mask.remove();
+        });
+
     }
-}
+};
